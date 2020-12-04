74a62c59e2c0a46cbb6ca9865b2df59359562289
Grant Sielman <69483686+gsielman@users.noreply.github.com>
2020-12-04 11:40:45 -0800

    Update numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #numbers.sh
 #Grant Sielman
+chmod 755 numbers.sh
 echo "Enter a positive number: "
 read NUMBER
 
 N=1
-chmod 755 numbers.sh
 echo "Enter a positive number: "
 read NUMBER
 while [ "$N" -le "$NUMBER" ]
